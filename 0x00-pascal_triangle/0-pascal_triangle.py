6e26930dd38845aa0d43e74589a5edcfa18e56f7
Basliel Tegegn <basgolilo5@gmail.com>
2024-06-27 09:20:13 -0700

    finalize'

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,17 +3,20 @@
 0. Pascal's Triangle
 
 Returns:
-    list: A list of lists of integers representing Pascal's triangle of n.
+    list: A list of lists of integers
+    representing Pascal's triangle of n.
 """
 
 def pascal_triangle(n):
     """Generate Pascal's Triangle up to the nth row.
 
     Args:
-        n (int): Number of rows of Pascal's Triangle to generate.
+        n (int): Number of rows of
+        Pascal's Triangle to generate.
 
     Returns:
-        list: A list of lists of integers representing Pascal's triangle of n.
+        list: A list of lists of integers
+        representing Pascal's triangle of n.
     """
     triangle = []
     if n > 0:
