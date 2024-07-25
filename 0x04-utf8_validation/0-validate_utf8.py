9674ab9d953f9599fff5ac71c15409dd9df4d9d6
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-25 00:56:51 -0700

    done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,32 +2,32 @@
 """UTF-8 Validation"""
 
 
-def get_leading_set_bits(num):
+def cal_le_1bit_8bit(num):
     """
     Calculate the number of leading 1 bits in an 8-bit integer.
     """
-    set_bits = 0
-    helper = 1 << 7
-    while helper & num:
-        set_bits += 1
-        helper = helper >> 1
-    return set_bits
+    bits = 0
+    loader = 1 << 7
+    while loader & num:
+        bits += 1
+        loader = loader >> 1
+    return bits
 
 
 def validUTF8(data):
     """
     Determine if the data represents a valid UTF-8 encoding.
     """
-    bits_count = 0
-    for i in range(len(data)):
-        if bits_count == 0:
-            bits_count = get_leading_set_bits(data[i])
-            if bits_count == 0:
+    count_the_bit = 0
+    for x in range(len(data)):
+        if count_the_bit == 0:
+            count_the_bit = cal_le_1bit_8bit(data[x])
+            if count_the_bit == 0:
                 continue
-            if bits_count == 1 or bits_count > 4:
+            if count_the_bit == 1 or count_the_bit > 4:
                 return False
         else:
-            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+            if not (data[x] & (1 << 7) and not (data[x] & (1 << 6))):
                 return False
-        bits_count -= 1
-    return bits_count == 0
+        count_the_bit -= 1
+    return count_the_bit == 0
