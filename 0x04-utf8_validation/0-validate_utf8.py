f253692f9bd3058c02474b702f824be2cd784290
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-25 00:51:44 -0700

    fix

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def get_leading_set_bits(num):
+    """
+    Calculate the number of leading 1 bits in an 8-bit integer.
+    """
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
+    return set_bits
+
+
+def validUTF8(data):
+    """
+    Determine if the data represents a valid UTF-8 encoding.
+    """
+    bits_count = 0
+    for i in range(len(data)):
+        if bits_count == 0:
+            bits_count = get_leading_set_bits(data[i])
+            if bits_count == 0:
+                continue
+            if bits_count == 1 or bits_count > 4:
+                return False
+        else:
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+                return False
+        bits_count -= 1
+    return bits_count == 0
