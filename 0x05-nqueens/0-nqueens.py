d0e95ac0d44c0ba1cec4024a998c574811d7cbcd
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-01 01:34:29 -0700

    ad

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,49 +1,40 @@
 #!/usr/bin/python3
-""" N QUEENS ALGORITHM WITH BACKTRACKING (RECURSION INSIDE LOOP) """
+
 import sys
 
 
 class NQueen:
-    """ Class for solving N Queen Problem """
-
+    """ Constructor to initialize the board size and other variables """
+    
     def __init__(self, n):
-        """ Global Variables """
+        
         self.n = n
         self.x = [0 for i in range(n + 1)]
         self.res = []
 
+    """Function to check if a queen can be placed at position (k, i)"""
     def place(self, k, i):
-        """ Checks if k Queen can be placed in i column (True)
-        or if the are attacking queens in row or diagonal (False)
-        """
-
-        # j checks from 1 to k - 1 (Up to previous queen)
         for j in range(1, k):
-            # There is already a queen in column
-            # or a queen in same diagonal
             if self.x[j] == i or \
                abs(self.x[j] - i) == abs(j - k):
                 return 0
         return 1
-
+    
+    """Recursive function to solve the N Queens problem"""
     def nQueen(self, k):
-        """ Tries to place every queen in the board
-        Args:
-        k: starting queen from which to evaluate (should be 1)
-        """
-        # i goes from column 1 to column n (1st column is 1st index)
+        
         for i in range(1, self.n + 1):
             if self.place(k, i):
-                # Queen can be placed in i column
+                
                 self.x[k] = i
                 if k == self.n:
-                    # Placed all 4 Queens (A solution was found)
+                    
                     solution = []
                     for i in range(1, self.n + 1):
                         solution.append([i - 1, self.x[i] - 1])
                     self.res.append(solution)
                 else:
-                    # Need to place more Queens
+                    
                     self.nQueen(k + 1)
         return self.res
 
