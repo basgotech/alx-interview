6982e279c22545430a43fc95bdd9272055e3dc12
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-01 01:52:15 -0700

    ad

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -19,11 +19,11 @@ class NQueen:
         """
 
         # j checks from 1 to k - 1 (Up to previous queen)
-        for j in range(1, k):
+        for a in range(1, k):
             # There is already a queen in column
             # or a queen in same diagonal
-            if self.x[j] == i or \
-               abs(self.x[j] - i) == abs(j - k):
+            if self.x[a] == i or \
+               abs(self.x[a] - i) == abs(a - k):
                 return 0
         return 1
 
@@ -31,17 +31,17 @@ class NQueen:
         """ Recursive function to solve the
         N Queens problem
         """
-        # i goes from column 1 to column n (1st column is 1st index)
-        for i in range(1, self.n + 1):
-            if self.place(k, i):
+        # a goes from column 1 to column n (1st column is 1st index)
+        for a in range(1, self.n + 1):
+            if self.place(k, a):
                 # Queen can be placed in i column
-                self.x[k] = i
+                self.x[k] = a
                 if k == self.n:
                     # If all queens are placed, add the
                     # solution to the result list
                     solution = []
-                    for i in range(1, self.n + 1):
-                        solution.append([i - 1, self.x[i] - 1])
+                    for a in range(1, self.n + 1):
+                        solution.append([a - 1, self.x[a] - 1])
                     self.res.append(solution)
                 else:
                     # Need to place more Queens
@@ -55,20 +55,20 @@ if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
 
-N = sys.argv[1]
+B = sys.argv[1]
 
 try:
-    N = int(N)
+    B = int(N)
 except ValueError:
     print("N must be a number")
     sys.exit(1)
 
-if N < 4:
+if B < 4:
     print("N must be at least 4")
     sys.exit(1)
 
 queen = NQueen(N)
-res = queen.nQueen(1)
+res_grap = queen.nQueen(1)
 
-for i in res:
-    print(i)
+for a in res_grap:
+    print(a)
