a329581f88fb96502e29c0acbb04ec43b18b8441
Basliel Tegegn <basgolilo5@gmail.com>
2024-08-22 08:41:06 -0700

    bas

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,40 +3,36 @@
 Making Change
 """
 
-def minimumCoins(coin_values, target_amount):
+
+def makeChange(coins, total):
     """
-    Return the minimum number of coins needed to meet a given total.
-    Args:
-        coin_values (list of ints): a list of coins of different values
-        target_amount (int): total value to be met
-    Return:
-        Number of coins or -1 if meeting the total is not possible
+    Return the minimum number of coins needed to meet a given total
     """
-    if target_amount <= 0:
+    if total <= 0:
         return 0
-    if coin_values == [] or coin_values is None:
+    if coins == [] or coins is None:
         return -1
     try:
-        coin_index = coin_values.index(target_amount)
+        n = coins.index(total)
         return 1
     except ValueError:
         pass
 
-    coin_values.sort(reverse=True)
-    total_coins = 0
-    for coin in coin_values:
-        if target_amount % coin == 0:
-            total_coins += int(target_amount / coin)
-            return total_coins
-        if target_amount - coin >= 0:
-            if int(target_amount / coin) > 1:
-                total_coins += int(target_amount / coin)
-                target_amount = target_amount % coin
+    coins.sort(reverse=True)
+    coin_count = 0
+    for x in coins:
+        if total % x == 0:
+            coin_count += int(total / x)
+            return coin_count
+        if total - x >= 0:
+            if int(total / x) > 1:
+                coin_count += int(total / x)
+                total = total % x
             else:
-                total_coins += 1
-                target_amount -= coin
-                if target_amount == 0:
+                coin_count += 1
+                total -= x
+                if total == 0:
                     break
-    if target_amount > 0:
+    if total > 0:
         return -1
-    return total_coins
+    return coin_count
